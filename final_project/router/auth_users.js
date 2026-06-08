adf9353da76789d62796798812589d01f4eae753
Krowma <antoine.david.n7@gmail.com>
2026-06-08 19:02:37 -0400

    add async calls

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -47,7 +47,7 @@ regd_users.post("/login", (req,res) => {
         req.session.authorization = {
             accessToken, username
         }
-        return res.status(200).json({ message: "User successfully logged in"});
+        return res.status(200).send({ message: "User successfully logged in"});
     } else {
         return res.status(208).json({ message: "Invalid Login. Check username and password" });
     }
@@ -57,13 +57,28 @@ regd_users.post("/login", (req,res) => {
 regd_users.put("/auth/review/:isbn", (req, res) => {
     const book = books[req.params.isbn];
     const review = req.params.review;
-    if(book && review)
+    const username = req.session.authorization['username'];
+    if(book && review && username)
     {
-        book.reviews[req.session.username] = review;
+        book.reviews[username] = review; 
         res.status(200).json({ message: "Review added successfully."});
     }
     else{
         res.status(404).json({ message: "Unable to find book."});
+    }
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const book = books[req.params.isbn];
+    const username = req.session.authorization['username'];
+    if(book && username)
+    {
+        delete book.reviews[username];
+        res.status(200).json({ message: "Review deleted successfully."});
+    }
+    else 
+    {
+        res.status(404).json({ message: "Unable to find review."});
     }
 });
 
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,27 +23,56 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books,null,4));
+    const getBooks = new Promise((resolve, reject) => {
+        resolve(books);
+    });
+
+    getBooks
+        .then((data) => res.send(JSON.stringify(books,null,4)))
+        .catch((err) => res.status(404).json({ message: err }));
+    
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    res.send(books[isbn]);
+    const getBooksByIsbn = new Promise((resolve, reject) => {
+        const isbn = req.params.isbn;
+        const book = books[isbn];
+        if (book) resolve(book);
+        else reject("Book not found");
+    }); 
+
+    getBooksByIsbn
+        .then((data) => res.json(data))
+        .catch((err) => res.status(404).json({ message: err })); 
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    const booksByAuthor = Object.values(books).filter(element => element.author === author);
-    res.send(booksByAuthor);
+    const getBooksByAuthor = new Promise((resolve, reject) => {
+        const author = req.params.author;
+        const filteredBooks = Object.values(books).filter(b => b.author === author);
+        if (filteredBooks.length > 0) resolve(filteredBooks);
+        else reject("No books found by this author");
+    });
+
+    getBooksByAuthor
+        .then((data) => res.json(data))
+        .catch((err) => res.status(404).json({ message: err }));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
-    const booksByTitle = Object.values(books).filter(element => element.title === title);
-    res.send(booksByTitle);
+    const getBooksByTitle = new Promise((resolve, reject) => {
+        const title = req.params.title;
+        const filteredBooks = Object.values(books).filter(b => b.title === title);
+        if (filteredBooks.length > 0) resolve(filteredBooks);
+        else reject("No books found by this title");
+    });
+
+    getBooksByTitle
+        .then((data) => res.json(data))
+        .catch((err) => res.status(404).json({ message: err }));
 });
 
 //  Get book review
